831a09894a0dd07f0264025fb3d1c2cf14add888
serafyn <38337920+serafyn@users.noreply.github.com>
2022-12-10 23:26:53 -0300

    up

--- 01-notas/src/views/home.php
+++ 01-notas/src/views/home.php
@@ -7,21 +7,20 @@ use Vidamrr\Notas\models\Note;
 $notes = Note::getAll();
 ?>
 
+<h1>Home</h1>
+<div class="textarea">
+    <?php
 
+    foreach ($notes as $note) {
+    ?>
+        <a href="?view=view&id=<?php echo $note->getUuid(); ?>">
+            <textarea><?php echo $note->getTitle(); ?></textarea>
+        </a>
 
+    <?php }
 
-<h1>home</h1>
-
-<?php
-
-foreach ($notes as $note) {
-?>
-
-
-<?php }
-
-?>
-
+    ?>
+</div>
 
 
 <?php
