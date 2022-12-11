c512c1bc126daedf959139a3f5ee8161a5ee6b6f
serafyn <38337920+serafyn@users.noreply.github.com>
2022-12-11 00:01:11 -0300

    Up

--- 01-notas/src/views/view.php
+++ 01-notas/src/views/view.php
@@ -1,8 +1,18 @@
 <?php
 require 'header.php';
+
+use Vidamrr\Notas\models\Note;
+
+if (isset($_GET['id'])) {
+
+    $note = Note::get($_GET['id']);
+} else {
+    header('Location:?view=home');
+}
+
 ?>
 
-<h1>view</h1>
+<h1>View</h1>
 
 <?php
 require 'footer.php';
